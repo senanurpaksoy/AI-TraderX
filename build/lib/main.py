a3acec71801f5c62560031ddf916c61f8b4c5a53
Ahmet Faruk Pala <65506131+afpthedev@users.noreply.github.com>
2023-08-01 16:10:44 +0300

    Readme düzeltildi.

---
+++ build/lib/main.py
@@ -0,0 +1,14 @@
+import Regression.Regression
+from Data import Stockdata
+
+
+def main():
+    print("Hello World")
+
+
+
+if __name__ == '__main__':
+    start_date = "2023-01-01"
+    end_date = "2023-07-31"
+    #Stockdata.get_stock_market_data_basic("AAPL",startDate=start_date,endDate=end_date)
+    Regression.Regression.RandomForestRegress("AAPL")
